613351ee43d653b4a3b32dc115bbd65ab13434f2
Nando <nando.96.25@gmail.com>
2018-12-20 18:07:06 +0100

    Upload BT_receiver with accelerometer data

--- BT_communication/BT-BT/BT_Receiver/BT_Receiver.ino
+++ BT_communication/BT-BT/BT_Receiver/BT_Receiver.ino
@@ -1,26 +1,50 @@
 
 #include<SoftwareSerial.h>
 
+#define WAITING_INIT 0
+#define WAITING_X 1
+#define WAITING_Y 2
 // se define al Pin2 como RX, Pin3 como TX
 SoftwareSerial mySerial(2,3);
 char c = ' ';
+char state;
+char x,y;
 
 void setup(){      
    Serial.begin(9600);
    mySerial.begin(38400);
+   state = WAITING_INIT;
 }
 
 void loop() {
         if(mySerial.available()>0){
           c = mySerial.read();
-          if(c<0){
-            //Serial.print("Error (<0) ");
-          }else{
-            //Serial.print("Alright (>0) ");
+          switch(state){
+                case WAITING_INIT:
+                    if (c==125)state=WAITING_X;
+                    break;
+                case WAITING_X:
+                    if(c==125){
+                      state = WAITING_X;
+                    }else{
+                      x=c;
+                      state = WAITING_Y;
+                    }
+                    break;
+                case WAITING_Y:
+                    if(c==125){
+                      state = WAITING_X;
+                    }else{
+                      y=c;
+                      state = WAITING_INIT;
+                      Serial.print("X: ");
+                      Serial.print((int)x);
+                      Serial.print("   Y: ");
+                      Serial.println((int)y);
+                    }
+                    break;
           }
           
-          //Serial.print("Recibido: ");
-          Serial.print(c);
           delay(5);
         }
 }
