263cfd9330a01a07293dcac38589861925071233
Guillermo Gil Rodriguez <guillegilrod@gmail.com>
2019-01-17 20:43:59 +0100

    Found missed Trasmitter files

--- BT_communication/BT-BT/BT_Transmitter/BT_Transmitter.ino
+++ BT_communication/BT-BT/BT_Transmitter/BT_Transmitter.ino
@@ -1,31 +1,60 @@
 
 #include <SoftwareSerial.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+ 
 #include "bt_utils.hpp"
 
 #define BT_RX     2
 #define BT_TX     3
 
+#define MPU_ADDR 0x68
+
 SoftwareSerial BTtransmiter(BT_RX,BT_TX);    // RX,TX
+MPU6050 mpu(MPU_ADDR);
 
-char *data = "Hello World\r\n";
+int ax, ay, az;
+int gx, gy, gz;
 
+char data[4] = {125,10, -10, '\0'};
+
+long txtime = 0;
 
 void setup()
 {  
   Serial.begin(9600);
   BTtransmiter.begin(38400);
+  Wire.begin();
+  mpu.initialize();
+  Serial.println(mpu.testConnection() ? F("IMU iniciado correctamente") : F("Error al iniciar IMU"));
+
+  
 }
 
 void loop()
 {
-  int s = write_stream(BTtransmiter, data);
-  delay(100);
+  mpu.getAcceleration(&ax, &ay, &az);
   
-  if(s<0){
-    Serial.print("Error("); Serial.print(s); Serial.print(") ");
-  }else{
-    Serial.print("Alright("); Serial.print(s); Serial.print(") ");
-    Serial.println("Enviado 'a'");
-  }
-  
+  //Calcular los angulos de inclinacion
+  float accel_ang_x = atan(ax / sqrt(pow(ay, 2) + pow(az, 2)))*(180.0 / 3.14);
+  float accel_ang_y = atan(ay / sqrt(pow(ax, 2) + pow(az, 2)))*(180.0 / 3.14);
+ 
+   if(millis() - txtime >  50)
+   {
+     static uint8_t i = 0;
+    
+     BTtransmiter.write(data[i]);
+
+     if(i == 2)
+     {
+      i = 0;
+      data[1] = (int)accel_ang_x;
+      data[2] = (int)accel_ang_y;
+     }else
+       ++i;
+      
+     txtime = millis();
+   }
+
 }
